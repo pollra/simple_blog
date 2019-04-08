0595d2327e53ac149b758e177f80c2b0089934e8
pollra32 <pollra32@naver.com>
2019-04-08 21:43:45 +0900

    Ip log Real ip print update bug complete (1/1)

--- src/main/java/com/pollra/intercepter/function/LoginInterceptor.java
+++ src/main/java/com/pollra/intercepter/function/LoginInterceptor.java
@@ -20,17 +20,17 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        log.info("[login] login / start.");
+//        log.info("[login] login / start.");
         request.setAttribute("ip",request.getHeader("x-real-ip"));
         log.info("[login] Access IP: "+ request.getAttribute("ip"));
-        HttpSession session = request.getSession();
-        try {
-            String loginUser = session.getAttribute("lu").toString();
-        }catch (NullPointerException e){
-            log.info("[login] login info is null. redirect loginPage");
-            session.setAttribute("lu","");
-            response.sendRedirect("/login/page");
-        }
+//        HttpSession session = request.getSession();
+//        try {
+//            String loginUser = session.getAttribute("lu").toString();
+//        }catch (NullPointerException e){
+//            log.info("[login] login info is null. redirect loginPage");
+//            session.setAttribute("lu","");
+//            response.sendRedirect("/login/page");
+//        }
 
         log.info("[login] login / end.");
         return super.preHandle(request, response, handler);
